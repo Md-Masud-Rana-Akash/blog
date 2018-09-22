9e50c40a8de919460591836f84747a737ef355ad
unknown <rana.akash0003@gmail.com>
2018-09-23 03:39:06 +0600

    Login and registration page created

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+ Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin' , 'middleware' => 'auth'] , function(){
+
+    // Route::get('/home', [
+
+    //     'uses' =>'HomeController@index',
+    //     'as' =>'home'
+    
+    // ]);
+
+    Route::get('/post/create', [
+        'uses'=>'PostsController@create',
+        'as'=>'post.create'
+    
+    ]);
+    
+    Route::post('/post/store', [
+        'uses'=>'PostsController@store',
+        'as'=>'post.store'
+    
+    ]);
+
+
+});
+
